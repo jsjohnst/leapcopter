9571b94cd33e333fa1d036ba7b69c474051ebeb8
Jeremy Johnstone <github@jeremyjohnstone.com>
2013-09-29 11:11:27 -0700

    updates to ui

--- js/leapcopter.js
+++ js/leapcopter.js
@@ -19,8 +19,59 @@ function debounce(func, wait, immediate) {
     };
 };
 
+function audioPlay() {
+    document.getElementById("music").play();
+}
+
+function audioStop() {
+    setTimeout(function() { document.getElementById("music").pause(); hideCameras(); }, 1000);
+}
+
+var entered_grid = false;
+var cameras_visible = 0;
+var camera_last_change = null;
+
+function showCamera(which) {
+    if(which != (cameras_visible+1)) return;
+    if(camera_last_change) {
+        if(Date.now() - camera_last_change < 2500) return;
+    }
+
+    camera_last_change = Date.now();
+
+    if(which == 1) {
+        if(!entered_grid) {
+            document.getElementById("cameras").className = "";
+        }
+    } else {
+        entered_grid = true;
+        document.getElementById("cameras").className = "grid";
+    }
+
+    document.getElementById("camera" + which).className += " show";
+
+    if(which == 3) {
+        document.getElementById("camera4").className += " show";
+        which = 4;
+    }
+
+    cameras_visible = which;
+}
+
+function hideCameras() {
+    for(var i = 1; i <= 4; i++) {
+        document.getElementById("camera" + i).className = "camera";
+    }
+}
+
+function updateText(cmd) {
+    document.getElementById("text").innerHTML = cmd;
+}
+
 function sendCommandImmediate(cmd) {
-    socket.emit(cmd);
+    console.log(cmd);
+    updateText(cmd); 
+    //socket.emit(cmd);
 }
 
 var sendCommand = debounce(function(cmd) {
@@ -36,7 +87,7 @@ function gestureRecognized(gesture, sub) {
         }
         return;
     } else {
-        if(gesture == "swipe" && sub) {
+        if(gesture == "swipe" && sub && sub != "up" && sub != "down") {
             sendCommand(sub);
         } else if(gesture == "spin" && sub) {
             sendCommandImmediate(sub);
@@ -105,6 +156,8 @@ function handleCircle(event, gesture) {
 var controllerOptions = {enableGestures: true};
 
 var lotsoffingers = false;
+var pointables_duration = 0;
+var started = false;
 
 Leap.loop(controllerOptions, function(event) {
    if (event.hands.length == 2) {
@@ -117,13 +170,32 @@ Leap.loop(controllerOptions, function(event) {
             } 
         }
 
-        if(event.pointables.length >= 6) {
+        if(event.pointables.length >= 8) {
             lotsoffingers = true;
-            setTimeout(function() { lotsoffingers = false; }, 2000); 
+            setTimeout(function() { lotsoffingers = false; }, 1000); 
         }
     
-        if(lotsoffingers && event.pointables.length <= 2) {
+        if(inflight && lotsoffingers && event.pointables.length <= 2) {
+            audioStop();
+            pointables_duration = 0;
+            lotsoffingers = false;
             return gestureRecognized("multi", "down");
+        }
+   }
+
+   if (event.hands.length == 1) {
+        if (event.pointables.length == 5) {
+            pointables_duration++;
+        }
+
+        if(pointables_duration > 100) {
+            started = true; 
+            showCamera(1);
+            audioPlay();
+        }
+
+        if (event.pointables.length > 1 && event.pointables.length < 5) {
+            showCamera(event.pointables.length);
         }
    }
 
