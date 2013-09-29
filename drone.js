9a4563fd8e808f24aae4964e148c5fca40e2be4c
Matthew Tom-Wolverton <mtw@mtwstudios.com>
2013-09-28 19:39:57 -0700

    rewrite in socket.io

--- drone.js
+++ drone.js
@@ -1,36 +1,49 @@
-var express = require('express');
-var arDrone = require('ar-drone');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
-var port = 3000;
-var app = express();
-
-/*
-client.takeoff();
-
-client
-  .after(500, function() {
-    this.clockwise(0.5);
-  })
-  .after(300, function() {
-    this.animate('flipLeft', 15);
-  })
-  .after(100, function() {
-    this.stop();
-    this.land();
-  });
-console.log("hi");
-*/
-
-app.get('/takeoff', function(req, res){
-  console.log("TAKEOFF!");
-  client.takeoff();
-});
-
-app.get('/land', function(req, res){
-  console.log("LAND!");
-  client.land();
-});
-
+var port = 3000
 app.listen(port);
-console.log("Listening on port " + port);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+function stop() {
+  setTimeout(function() { client.stop(); console.log("done"); }, 1000);
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.on('takeoff', function (data) {
+    console.log('TAKEOFF!');
+    client.takeoff();
+  });
+
+  socket.on('land', function (data) {
+    console.log('LANDING!');
+    client.land();
+  });
+
+  socket.on('right', function (data) {
+    console.log('right');
+    client.right(0.05);
+    stop();
+  });
+
+  socket.on('left', function (data) {
+    console.log('left');
+    client.left(0.05);
+    stop();
+  });
+});
